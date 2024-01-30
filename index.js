13ea722cf409a2550fec2b96fc2fdedba466cc1b
Aulex <117493012+au-lex@users.noreply.github.com>
2024-01-30 02:22:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }
+   app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
